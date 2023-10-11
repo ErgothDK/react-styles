04771154e4e540b322285efb17b8f9644b5fdb90
ErgothDK <ergothdk@gmail.com>
2023-10-11 16:06:01 -0600

    - Changed styled component css to module css

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,34 +1,7 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import styled from "styled-components";
-
-const FormControls = styled.div`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.haserror ? "red" : "black")};
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid #ccc;
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-    border-color: ${(props) => (props.haserror ? "red" : "#ccc")};
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+import styles from "./CourseInput.module.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -52,10 +25,12 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControls haserror={!isValid}>
+      <div
+        className={`${styles["form-control"]}  ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControls>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
